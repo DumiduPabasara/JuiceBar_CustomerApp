cde22c0abf55fc86acbf51f09f275ead08e91ac7
Pabasara <pabasara@bluelotus360.com>
2021-06-05 16:16:33 +0530

    Understanding XAML Type Conversion

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -18,7 +18,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-//Learning 3
+//Learning 4
 
 namespace SexyJuiceBar_CustomerApp
 {
