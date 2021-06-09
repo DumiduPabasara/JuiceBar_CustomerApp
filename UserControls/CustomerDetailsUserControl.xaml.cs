c24e492799d38b1a3021cb85e9c3ff3b1268d4df
Pabasara <pabasara@bluelotus360.com>
2021-06-09 19:37:41 +0530

    using data bindings in user controls

--- UserControls/CustomerDetailsUserControl.xaml.cs
+++ UserControls/CustomerDetailsUserControl.xaml.cs
@@ -27,9 +27,12 @@ namespace SexyJuiceBar_CustomerApp.UserControls
         //private Customer _customer;
 
         // Using a DependencyProperty as the backing store for Customer.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty CustomerProperty =
+        //    DependencyProperty.Register("Customer", typeof(Customer), typeof(CustomerDetailsUserControl),
+        //        new PropertyMetadata(null, CustomerChangedCallBack));
         public static readonly DependencyProperty CustomerProperty =
             DependencyProperty.Register("Customer", typeof(Customer), typeof(CustomerDetailsUserControl),
-                new PropertyMetadata(null, CustomerChangedCallBack));
+                new PropertyMetadata(null));
 
         public CustomerDetailsUserControl()
         {
@@ -60,42 +63,42 @@ namespace SexyJuiceBar_CustomerApp.UserControls
 
         
 
-        private static void CustomerChangedCallBack(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            if(d is CustomerDetailsUserControl customerDetailsUserControl)
-            {
-                var customer = e.NewValue as Customer
-                customerDetailsUserControl.txtFirstName.Text = customer?.FirstName ?? "";
-                customerDetailsUserControl.txtLastName.Text = customer?.LastName ?? "";
-                customerDetailsUserControl.txtEmail.Text = customer?.Email ?? "";
-                customerDetailsUserControl.txtPhone.Text = customer?.TeleNo ?? "";
-                customerDetailsUserControl.chkAlcohol.IsChecked = customer?.IsAlcoholUser;
+        //private static void CustomerChangedCallBack(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        //{
+        //    if(d is CustomerDetailsUserControl customerDetailsUserControl)
+        //    {
+        //        var customer = e.NewValue as Customer
+        //        customerDetailsUserControl.txtFirstName.Text = customer?.FirstName ?? "";
+        //        customerDetailsUserControl.txtLastName.Text = customer?.LastName ?? "";
+        //        customerDetailsUserControl.txtEmail.Text = customer?.Email ?? "";
+        //        customerDetailsUserControl.txtPhone.Text = customer?.TeleNo ?? "";
+        //        customerDetailsUserControl.chkAlcohol.IsChecked = customer?.IsAlcoholUser;
 
-            }
+        //    }
 
-        }
+        //}
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            UpdateCustomer();
-        }
+        //private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        //{
+        //    UpdateCustomer();
+        //}
 
-        private void CheckBox_IsCheckedChanged(object sender, RoutedEventArgs e)
-        {
-            UpdateCustomer();
-        }
+        //private void CheckBox_IsCheckedChanged(object sender, RoutedEventArgs e)
+        //{
+        //    UpdateCustomer();
+        //}
 
-        private void UpdateCustomer()
-        {
-            //var customer = customerListView.SelectedItem as Customer;
-            if (Customer != null)
-            {
-                Customer.FirstName = txtFirstName.Text;
-                Customer.LastName = txtLastName.Text;
-                Customer.Email = txtEmail.Text;
-                Customer.TeleNo = txtPhone.Text;
-                Customer.IsAlcoholUser = chkAlcohol.IsChecked.GetValueOrDefault();
-            }
-        }
+        //private void UpdateCustomer()
+        //{
+        //    //var customer = customerListView.SelectedItem as Customer;
+        //    if (Customer != null)
+        //    {
+        //        Customer.FirstName = txtFirstName.Text;
+        //        Customer.LastName = txtLastName.Text;
+        //        Customer.Email = txtEmail.Text;
+        //        Customer.TeleNo = txtPhone.Text;
+        //        Customer.IsAlcoholUser = chkAlcohol.IsChecked.GetValueOrDefault();
+        //    }
+        //}
     }
 }
